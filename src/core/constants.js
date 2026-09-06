52643357ea01475d0c79ebaba26290f9d59fc307
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-09-06 13:04:24 +0100

    feat: add core constants

---
+++ src/core/constants.js
@@ -0,0 +1,10 @@
+export const SECONDS_PER_DAY = 86400;
+export const SCHEMA_VERSION = 1;
+export const DEFAULT_SETTINGS = Object.freeze({
+  inactivityDays: 3,
+  maxAddiction: 3,
+  prioritizeNeverTrained: true,
+  showGlobalBadge: true,
+  showTrainCount: true,
+  refreshMinutes: 5
+});
